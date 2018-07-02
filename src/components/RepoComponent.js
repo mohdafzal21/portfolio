ee7bafab6a780f1c9daf8d4d7fb7cc5e55e79fc7
Umang <umangpahwa@gmail.com>
2018-07-02 19:41:05 +0530

    repo component edited

--- src/components/RepoComponent.js
+++ src/components/RepoComponent.js
@@ -1,13 +1,29 @@
 import styles from '../stylesheets/aboutMe.css'
 
-const RepoComponent = ({name, html_url, updated_at, language}) =>
-    <div className="ui segment">
-        <h3>{name}</h3>
-        <div className={styles.inLine}>
-            <div className={styles.gitCircle}/>
-            <div className={`column`}>{language}</div>
-            <div className={`column ${styles.updated}`}>{updated_at}</div>
+const randomColor = () => '#' + [...Array(6)].map(() => parseInt(Math.random() * 16).toString(16)).join('')
+
+const RepoComponent = ({name, html_url, updated_at, language}) => {
+
+    const updateDate = new Date(updated_at)
+
+    const gitCircle = {
+
+        background: randomColor(),
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%'
+    }
+
+    return (
+        <div className="ui segment">
+            <h3>{name}</h3>
+            <div className={styles.inLine}>
+                <div style={gitCircle}/>
+                <div className={`column`}>{language}</div>
+                <div className={`column ${styles.updated}`}>{updateDate.toLocaleDateString()}</div>
+            </div>
         </div>
-    </div>
+    )
+}
 
 export default RepoComponent
